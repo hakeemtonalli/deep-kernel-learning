83132cd84a75a333f9c5202666b4f32ec1e057fa
hakeemtfrank <hakeemtfrank@gmail.com>
2020-12-06 18:11:23 -0800

    wrote LSTM and GRU feature extractors

--- deepkl/models.py
+++ deepkl/models.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import gpytorch
 
 class SpectralMixtureGPModel(gpytorch.models.ExactGP):
@@ -20,7 +21,6 @@ class SpectralMixtureGPModel(gpytorch.models.ExactGP):
         super(SpectralMixtureGPModel, self).__init__(train_x, train_y, likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
         self.covar_module = gpytorch.kernels.SpectralMixtureKernel(num_mixtures=num_mixtures)
-        #self.covar_module.initialize_from_data(train_x, train_y)
         self.covar_module.initialize_from_data_empspect(train_x, train_y)
 
     def forward(self, x):
@@ -29,5 +29,29 @@ class SpectralMixtureGPModel(gpytorch.models.ExactGP):
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
     
     
-
+class GRUFeatureExtractor(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(GRUFeatureExtractor, self).__init__()
+        self.hidden_size = hidden_size 
+        self.num_layers = num_layers
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
         
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, _ = self.gru(x, h0)
+        out = out.reshape(out.shape[0], -1)
+        return out
+        
+class LSTMFeatureExtractor(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTMFeatureExtractor, self).__init__()
+        self.hidden_size = hidden_size 
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=False)
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        out, _ = self.lstm(x, (h0, c0))
+        out = out.reshape(out.shape[0], -1)
+        return out
